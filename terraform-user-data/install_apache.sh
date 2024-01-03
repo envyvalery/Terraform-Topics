8b2872347e7530647cdc9ce791b610c2d2aa86d3
Ngah Valery Nkarakwi <140969141+envyvalery@users.noreply.github.com>
2024-01-02 21:36:18 -0600

    Add files via upload

---
+++ terraform-user-data/install_apache.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt install python3
+mkdir basic-http-server
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
---
+++ terraform-variable-tf-tfvars/variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+  type = string
+  description = "Project region location"
+}
+variable "instance_type" {
+  type = string
+  description = "EC2 Instance type"
+}
+variable "tag" {
+  type = string
+  description = "Tag for EC2 instance"
+}
