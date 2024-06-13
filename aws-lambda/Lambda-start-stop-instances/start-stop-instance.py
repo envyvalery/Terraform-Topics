b40173f7c59de18f12c9d0cf6a41979fce8c199c
Ngah Valery Nkarakwi <140969141+envyvalery@users.noreply.github.com>
2024-06-13 03:21:36 -0500

    Update start-stop-instance.py

--- aws-lambda/Lambda-start-stop-instances/start-stop-instance.py
+++ aws-lambda/Lambda-start-stop-instances/start-stop-instance.py
@@ -1,20 +1,21 @@
 #This block stops the instances
 
 import boto3
-region = 'us-west-1'
+region = 'us-east-1'
 instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
 ec2 = boto3.client('ec2', region_name=region)
 
+#Funnction
 def lambda_handler(event, context):
     ec2.stop_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
     
  #This block starts instances    
 import boto3
-region = 'us-west-1'
+region = 'us-east-1'
 instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
 ec2 = boto3.client('ec2', region_name=region)
-
+#function
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instances)
     print('started your instances: ' + str(instances))
